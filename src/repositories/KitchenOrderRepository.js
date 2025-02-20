d4480380c60da3a6566e77e04fa27b102fc2fa13
Muhammad Shabi Ul Akber <shabiulakber@gmail.com>
2025-02-21 03:25:19 +0500

    make another repository

---
+++ src/repositories/KitchenOrderRepository.js
@@ -0,0 +1,30 @@
+const IRepository = require("./IRepository");
+const KitchenOrder = require("../models/KitchenOrders");
+
+class KitchenOrderRepository extends IRepository {
+  constructor() {
+    super(KitchenOrder);
+  }
+
+  async getAll(query) {
+    return await KitchenOrder.findAll({ where: { KitOrd_BranchFK: query.BranchId, IsDeleted: false }, include: ["Branch", "Order", "KitchenSection"] });
+  } 
+
+  async getById(id) {
+    return await KitchenOrder.findByPk(id, { include: ["Branch", "Order", "KitchenSection"] });
+  }
+
+  async create(data) {
+    return await KitchenOrder.create(data);
+  }
+
+  async update(id, data) {
+    return await KitchenOrder.update(data, { where: { KitOrd_PK: id } });
+  }
+
+  async delete(id) {
+    return await KitchenOrder.update({ IsDeleted: true }, { where: { KitOrd_PK: id } });
+  }
+}
+
+module.exports = new KitchenOrderRepository();
